be876c2450739be20561c092966c78c3d7798335
rajarshi433 <mannarajarshi65@gmail.com>
2024-11-24 00:10:48 +0530

    set up prisma and webhook

---
+++ apps/frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <Button variant="destructive" className="bg-green-900 text-white">Click me</Button>
+    </div>
+  );
+}

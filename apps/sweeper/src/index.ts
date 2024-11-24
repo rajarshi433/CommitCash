04e852ff60bf1d0a1e22b03ccebb565176d6125d
Turbobot <turbobot@vercel.com>
2024-11-24 20:07:58 +0530

    feat(create-turbo): apply official-starter transform

---
+++ apps/sweeper/src/index.ts
@@ -0,0 +1,32 @@
+import express, { Express } from "express";
+import "dotenv/config";
+import prisma from "@repo/database/client";
+
+const app: Express = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.send({
+    message: "Hello from sweeper",
+  });
+});
+
+const PORT = process.env.PORT || 8001;
+
+const startServer = async () => {
+  try {
+    await prisma.$connect();
+    console.log("Database connection successful");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Database connection failed", error);
+    process.exit(1);
+  }
+};
+
+startServer();
